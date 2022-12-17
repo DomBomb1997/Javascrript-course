862cf701afbbdeba313059171541c0208fe922d0
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2022-12-17 02:31:56 -0800

    Submitted for grading

---
+++ Project2_functions/java.js
@@ -0,0 +1,9 @@
+function changeColor(newColor) {
+    const elem = document.getElementById('para');
+    elem.style.color = newColor; }
+
+function myfunction() {
+    var sentence = "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
