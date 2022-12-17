f10edf478208f0b5dd04bf888d7982c9bb4961f1
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2022-12-17 02:33:36 -0800

    Submitted for grading

--- Project2_functions/java.js
+++ Project2_functions/java.js
@@ -1,7 +1,8 @@
+//Color changing function//
 function changeColor(newColor) {
     const elem = document.getElementById('para');
     elem.style.color = newColor; }
-
+//click me function//
 function myfunction() {
     var sentence = "I am learning";
     sentence += "a lot from this book!";
