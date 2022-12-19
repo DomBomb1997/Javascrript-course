9222f5f0a4ec69ca5555e0e5a27719727e30f6b0
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2022-12-19 15:20:49 -0800

    Submitted for help

--- Basic JavaScript Projects/project 3/JS/main.js
+++ Basic JavaScript Projects/project 3/JS/main.js
@@ -1,4 +1,50 @@
 function addition_Function() {
-    var additiom = 2 + 2;
+    var addition = 2 + 2;
     document.getElementById("Math").innerHTML = "2 + 2 =" + addition;
 }
+
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("Math2").innerHTML = "5 - 2 =" + subtraction;
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math3").innerHTML = "6 X 8 = " + simple_Math;
+}
+
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Math4").innerHTML = "48 / 6 =" + simple_Math
+}
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10 ,divided in half and then subtracted by 5 equals" + simple_Math;
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a reminder of: " + simple_Math;
+}
+
+function negation_Operation() {
+    var X = 10;
+    document.getElementById("Math7").innerHTML = -X;
+}
+
+
+    var X = 5;
+    X++;
+    document.write(X);
+
+    var D = 5.25;
+    D--;
+    document.write(D)
+
+    window.alert (Math.random() * 100);
+
+   function Math_Object {
+    Math.round(4.6);
+    document.getElementById("Math8").innerHTML = txt;
+   }
