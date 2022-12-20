0f8b631cb24cbc0dccc1e1fd70be8ba3f8822976
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2022-12-20 00:55:18 -0800

    Submitting for grading

--- Basic JavaScript Projects/project 3/JS/main.js
+++ Basic JavaScript Projects/project 3/JS/main.js
@@ -1,38 +1,39 @@
+// Addition //
 function addition_Function() {
     var addition = 2 + 2;
     document.getElementById("Math").innerHTML = "2 + 2 =" + addition;
 }
-
+// Subtraction //
 function subtraction_Function() {
     var subtraction = 5 - 2;
     document.getElementById("Math2").innerHTML = "5 - 2 =" + subtraction;
 }
-
+// Multiplication //
 function multiplication() {
     var simple_Math = 6 * 8;
     document.getElementById("Math3").innerHTML = "6 X 8 = " + simple_Math;
 }
-
+// Division //
 function division() {
     var simple_Math = 48 / 6;
     document.getElementById("Math4").innerHTML = "48 / 6 =" + simple_Math
 }
-
+// Multiple function //
 function more_Math() {
     var simple_Math = (1 + 2) * 10 / 2 -5;
     document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10 ,divided in half and then subtracted by 5 equals" + simple_Math;
 }
-
+// Percentages //
 function modulus_Operator() {
     var simple_Math = 25 % 6;
     document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a reminder of: " + simple_Math;
 }
-
+// Negation //
 function negation_Operation() {
     var X = 10;
     document.getElementById("Math7").innerHTML = -X;
 }
-
+// Increment and Decrement //
 
     var X = 5;
     X++;
@@ -43,8 +44,8 @@ function negation_Operation() {
     document.write(D)
 
     window.alert (Math.random() * 100);
+// Math Object //
+   function Math_Object() {
 
-   function Math_Object {
-    Math.round(4.6);
-    document.getElementById("Math8").innerHTML = txt;
+    document.getElementById("Math8").innerHTML = Math.round(4.6);
    }
