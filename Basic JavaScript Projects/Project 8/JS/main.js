3f1430bb759288d4d2583514b8ff7fb0e67db024
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2022-12-21 18:39:36 -0800

    Submitted for grading

---
+++ Basic JavaScript Projects/Project 8/JS/main.js
@@ -0,0 +1,35 @@
+function full_Sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence.";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML =whole_sentence
+}
+
+function slice_Method() {
+    var sentence = "All work and no play makes johnny a dull boy.";
+    var Section = sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.toUpperCase());
+
+function search_function() {
+    let text = "Mr. Blue has a blue house"
+let position = text.search("Blue");
+
+document.getElementById("search").innerHTML = position;
+}
+
+function string_Method() {
+    var X =182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function precision_Method() {
+    var X = 2374.82758378524;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
