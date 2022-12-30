c3f7c4ed624281281b766ff57ad792eb422b8183
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2022-12-30 02:28:37 -0800

    Submitted for help

--- Basic JavaScript Projects/Javascript Projects/Javascript/Javascript_2.js
+++ Basic JavaScript Projects/Javascript Projects/Javascript/Javascript_2.js
@@ -1,5 +1,8 @@
 function validateForm() {
-    let x = document.forms["myForm"]["text"].value;
+    let x = document.forms["myForm"]["fname"].value;
+    let x = document.forms["myForm"]["lname"].value;
+    let x = document.forms["myForm"]["email"].value;
+    let x = document.forms["myForm"]["phone"].value;
     if (x == "") {
       alert("Name must be filled out");
       return false;
