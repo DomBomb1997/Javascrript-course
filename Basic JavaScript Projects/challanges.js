af069bb7837110f4768ea8d0dcfd376ed3fb99bb
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2022-12-30 11:10:28 -0800

    Submitted for help

---
+++ Basic JavaScript Projects/challanges.js
@@ -0,0 +1,14 @@
+myfunction = function() {
+    return "This is my function";
+}
+A = "Always ";
+B = "remember: ";
+document.write(A + B);
+
+X = "you ";
+Y = "are ";
+document.write(X + Y);
+
+M = "unique, " ;
+N = "just like everyone else. ";
+document.write(N + M);
